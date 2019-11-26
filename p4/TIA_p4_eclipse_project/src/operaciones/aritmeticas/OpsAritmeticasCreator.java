e7e256b3ae4dc4705a63857231a63c030127aba3
luiserh1 <luis.serranohernandez.1415@gmail.com>
2019-11-26 14:22:10 +0100

    Creada la clase Creator

--- p4/TIA_p4_eclipse_project/src/operaciones/aritmeticas/OpsAritmeticasCreator.java
+++ p4/TIA_p4_eclipse_project/src/operaciones/aritmeticas/OpsAritmeticasCreator.java
@@ -1,5 +1,22 @@
 package operaciones.aritmeticas;
 
-public class OpsAritmeticasCreator {
+import java.util.Random;
 
+import org.opt4j.core.genotype.SelectGenotype;
+import org.opt4j.core.gproblem.creator;
+
+public class OpsAritmeticasCreator implements Creator<SelectGenotype<MathematicalSymbol>>
+{
+	public SelectGenotype<MathematicalSymbol> create()
+	{
+		MathematicalSymbol[] Symbols = {MathematicalSymbol.PLUS, MathematicalSymbol.MINUS,
+				MathematicalSymbol.MULTIPLICATION, MathematicalSymbol.DIVISION};
+		
+		SelectGenotype<MathematicalSymbol> genotype = new SelectGenotype<MathematicalSymbol>(Symbols);
+		// El genoripo estará formado por "numeroSimbolos" matematicos elegidos al azar
+		// En nuestro caso la población será un conjunto de individuos, donde cada individuo son 5 símbolos
+		genotype.init(new Random(), Data.numeroSimbolos);
+		
+		return genotype;
+	}
 }
