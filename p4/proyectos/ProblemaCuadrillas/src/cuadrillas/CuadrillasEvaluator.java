3c0b852367c46c44bc9f5438be36832f7f68682c
edoelas <miego.cvcv@gmail.com>
2020-01-02 21:26:28 +0100

    acasdasdasdad

--- p4/proyectos/ProblemaCuadrillas/src/cuadrillas/CuadrillasEvaluator.java
+++ p4/proyectos/ProblemaCuadrillas/src/cuadrillas/CuadrillasEvaluator.java
@@ -13,17 +13,18 @@ public class CuadrillasEvaluator implements Evaluator<ArrayList<Integer>>
 			int resultado = 0;
 			int coste = 0;
 			int productividad = 0;
+			int c1 = 0, c2 = 0, c3 = 0;
 			//int descanso = 0;
 
 			
 			for (int i = 0; i < phenotype.size(); ++i)
 			{
+
 				switch (phenotype.get(i))
 				{
-				case 1: coste += Data.turno1[i+1]; productividad += Data.productividad1[i+1]; break;
-				case 2: coste += Data.turno2[i+1]; productividad += Data.productividad2[i+1]; break;
-				case 3: coste += Data.turno3[i+1]; productividad += Data.productividad3[i+1]; break;
-				//case 0: descanso++; break;
+				case 1: coste += Data.turno1[i+1]; productividad += Data.productividad1[i+1]; c1++; break;
+				case 2: coste += Data.turno2[i+1]; productividad += Data.productividad2[i+1]; c2++; break;
+				case 3: coste += Data.turno3[i+1]; productividad += Data.productividad3[i+1]; c3++; break;
 				}
 			}
 			
@@ -33,12 +34,17 @@ public class CuadrillasEvaluator implements Evaluator<ArrayList<Integer>>
 			}
 			*/
 			
-			resultado = coste - productividad;
+			if (c1<3 || c2<3 || c3<3) {
+				coste=Integer.MAX_VALUE;
+				productividad = Integer.MIN_VALUE;
+			}
 			
 			
-	Objectives objectives = new Objectives();
-	objectives.add("Valor objetivo-MIN",  Sign.MIN, Math.abs(resultado));
-	return objectives;
+			
+			Objectives objectives = new Objectives();
+			objectives.add("Valor coste-MIN",  Sign.MIN, Math.abs(coste));
+			objectives.add("Valor productividad-MAX", Sign.MAX, Math.abs(productividad));
+			return objectives;
 	}
 }
 
